73041a0ff61c0542c1019a663852fd3338c6ddb4
Alveona <pomavau@yandex.ru>
2019-03-30 17:39:40 +0100

    POST and GET on /api/patients

--- backend/equeue/serializers.py
+++ backend/equeue/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import Doctor
+from .models import Doctor, Patient
 from django.conf import settings
+
 class DoctorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Doctor
         fields = ('name', 'works_from', 'works_to', 'specialisation')
+        
+class PatientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Patient
+        fields = ('name', 'gender', 'year_of_birth')
