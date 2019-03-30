cf819650f13b4433e653a81b8a0fee7cc5e67106
AlexFSmirnov <alex.f.smirnov@gmail.com>
2019-03-30 13:52:04 +0100

    get_diagnosis(symptoms, gender, year_of_birth)

--- python-api/api.py
+++ python-api/api.py
@@ -1,3 +1,4 @@
+import json
 import requests
 
 
@@ -6,14 +7,28 @@ class SymptomAPI:
     url = "https://priaid-symptom-checker-v1.p.rapidapi.com/"
     lang = "en-gb"
 
+    headers = {"X-RapidAPI-Key": key}
     
     def get_symptoms(self):
-        headers = {"X-RapidAPI-Key": self.key}
         data = {"language": self.lang, "format": "json"}
 
-        r = requests.get(self.url + "symptoms", headers=headers, params=data)
+        r = requests.get(self.url + "symptoms", 
+                         headers=self.headers, 
+                         params=data)
 
         return r.json()
+
+    def get_diagnosis(self, symptoms, gender, year_of_birth):
+        data = {"language": self.lang, "format": "json",
+                "symptoms": json.dumps(symptoms), "gender": gender,
+                "year_of_birth": str(year_of_birth)}
+
+        r = requests.get(self.url + "diagnosis", 
+                         headers=self.headers, 
+                         params=data)
+
+        return r.json()
+
 
     def set_language(self, new_lang):
         valid = ["de-ch", "en-gb", "fr-fr", "es-es", "tr-tr"]
