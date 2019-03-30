b65dc310a0ce9413dc17b61674613d1f7e1857b9
Ivan Ivashnyov <ivashnev@phystech.edu>
2019-03-30 17:29:56 +0100

    added qrcode creating

---
+++ qrcode_api/qrcode_api.py
@@ -0,0 +1,24 @@
+import qrcode
+
+
+"""
+    patient_id - айди пацента или любая информация, которую хотим сохранить (до 300 символов)
+    save_path - путь сохранения файла с его именем
+"""
+
+
+def createCode(patient_id, save_path):
+
+    qr = qrcode.QRCode(
+        version = 1,
+        error_correction = qrcode.constants.ERROR_CORRECT_H,
+        box_size = 10,
+        border = 4,
+    )
+
+    qr.add_data(patient_id)
+    qr.make(fit=True)
+
+    img = qr.make_image()
+
+    img.save(save_path)
