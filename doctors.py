b2640212d9a851178fb3b5a7450f44a3a733256d
Anton Karazeev <anton.karazeev@gmail.com>
2019-03-30 15:10:37 +0100

    Initial commit

---
+++ doctors.py
@@ -0,0 +1,44 @@
+import numpy as np
+import json
+
+def random_wh():
+    '''Returns working hours (from, to), 24h format.
+    '''
+    t1 = np.random.randint(1, 23)
+    t2 = t1 + np.random.randint(1, 24 - t1)
+    return t1, t2
+
+
+def random_specs(filename="data/specialisations.json"):
+    with open(filename, 'r') as f:
+        specialisations = json.load(f)
+
+    num_specs = np.random.randint(1, 7)
+    specialisations = np.random.choice(specialisations, num_specs)
+    return specialisations
+
+
+def random_name(topnames_filename='data/topnames.txt', topsurnames_filename='data/topsurnames.txt'):
+    with open(topnames_filename, 'r') as f:
+        topnames = f.readlines()
+
+    with open(topsurnames_filename, 'r') as f:
+        topsurnames = f.readlines()
+
+    topsurnames = [x.strip() for x in topsurnames]
+    topnames = [x.strip() for x in topnames]
+
+    return np.random.choice(topnames) + ' ' + np.random.choice(topsurnames)
+
+
+class Doctor:
+    def __init__(self):
+        self.name = random_name()
+        self.wh = random_wh()  # Working Hours.
+        self.specs = random_specs()
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return "{name} - {specs}".format(name=self.name, specs=' | '.join([x['Name'] for x in self.specs]))
