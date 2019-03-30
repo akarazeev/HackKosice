987bbe6f4593523f933ffb2d8146121f3aa3ca71
Anton Karazeev <anton.karazeev@gmail.com>
2019-03-30 21:18:11 +0100

    Up doctors generator

--- doctors.py
+++ doctors.py
@@ -9,15 +9,6 @@ def random_wh():
     return t1, t2
 
 
-def random_specs(filename="data/specialisations.json"):
-    with open(filename, 'r') as f:
-        specialisations = json.load(f)
-
-    num_specs = np.random.randint(1, 7)
-    specialisations = np.random.choice(specialisations, num_specs)
-    return specialisations
-
-
 def random_name(topnames_filename='data/topnames.txt', topsurnames_filename='data/topsurnames.txt'):
     with open(topnames_filename, 'r') as f:
         topnames = f.readlines()
@@ -35,10 +26,29 @@ class Doctor:
     def __init__(self):
         self.name = random_name()
         self.wh = random_wh()  # Working Hours.
-        self.specs = random_specs()
+        self.specs = list()
+        self.specs_only = list()
+
+    def update_specs(self):
+        self.specs_only = [x['Name'] for x in self.specs]
 
     def __str__(self):
         return self.name
 
     def __repr__(self):
         return "{name} - {specs}".format(name=self.name, specs=' | '.join([x['Name'] for x in self.specs]))
+
+ndoctors = 100
+doctors = [Doctor() for _ in range(ndoctors)]
+
+filename="data/specialisations.json"
+with open(filename, 'r') as f:
+    specialisations = json.load(f)
+
+for spec in specialisations:
+    chosen_doctors = np.random.choice(np.arange(len(doctors)), np.random.randint(1, 5))
+    for i in chosen_doctors:
+        doctors[i].specs.append(spec)
+
+for i in range(len(doctors)):
+    doctors[i].update_specs()
