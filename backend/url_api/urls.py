a27911c96fe4cdf918e7217292fcbf4ff3e0a58a
Alveona <pomavau@yandex.ru>
2019-03-30 14:48:05 +0100

    Implement GET /api/symptoms

--- backend/url_api/urls.py
+++ backend/url_api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import DiagnosisView
+from .views import DiagnosisView, SymptomView
 app_name = "url_api"
 
 urlpatterns = [
-    path('diagnosis', DiagnosisView.as_view())
+    path('diagnosis', DiagnosisView.as_view()),
+    path('symptoms', SymptomView.as_view())
 ]
--- backend/url_api/views.py
+++ backend/url_api/views.py
@@ -8,9 +8,21 @@ from rest_framework.response import Response
 from python_api import api
 # Create your views here.
 
+
+class SymptomView(APIView):
+    symptom_API = api.SymptomAPI()
+    def get(self, request):
+        symptoms = self.symptom_API.get_symptoms()
+        return Response(symptoms)
+
 class DiagnosisView(APIView):
+    symptom_API = api.SymptomAPI()
     def post(self, request):
-        data = request.data.get('ids')
-        print(data)
-        diagnosis = api.return_diagnosis(data)
-        return Response({"Diagnosis": diagnosis})
+        symptoms = request.data.get('symptoms')
+        # print(symptoms)
+        gender = request.data.get('gender')
+        year_of_birth = request.data.get('year_of_birth')
+        # print(request.data)
+        diagnosis = self.symptom_API.get_diagnosis(symptoms, gender, year_of_birth)
+        # print(diagnosis)
+        return Response(diagnosis)
