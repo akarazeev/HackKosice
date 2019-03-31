37d6094d49986defb16567c07ec6abf125890249
AlexFSmirnov <alex.f.smirnov@gmail.com>
2019-03-31 06:09:37 +0200

    Trying to make things pretty

--- doctors_front/js/script.js
+++ doctors_front/js/script.js
@@ -1,32 +1,47 @@
 function show_history(data, div_id) {
     var list = document.getElementById(div_id);
     list.innerHTML = "";
-    var name = document.createElement('div');
-    name.innerHTML = data.name;
-    var gender = document.createElement('div');
-    gender.innerHTML = data.gender;
-    var year = document.createElement('div');
-    year.innerHTML = data.year_of_birth;
-    list.appendChild(name);
-    list.appendChild(gender);
-    list.appendChild(year);
+    list.style = "font-size: 16pt";
+
+    var general = document.createElement('div');
+    general.innerHTML = "<strong>" + data.name + "</strong>, " + data.gender + ", " + data.year_of_birth + " year of birth.<br><br>";
+
+    var applied_on = document.createElement('div');
+    applied_on.innerHTML = "Patient applied " + data.applies.length + " time" + (data.applies.length == 1 ? "" : "s") + ": ";
+
+    list.appendChild(general);
+    list.appendChild(applied_on);
+
+    console.log(data);
+
+    var applies = document.createElement('ol');
+    var text = "";
+
     for (let i = 0; i < data.applies.length; ++i) {
-        var apply = document.getElementById("div");
-        //apply.innerHTML = "Apply";
-        var date = document.getElementById("div");
-        date.innerHTML = data.applies[i].date;
-        var symptoms = document.getElementById("div");
-        symptoms.innerHTML = data.applies[i].symptoms;
-        var diagnosis = document.getElementById("div");
-        diagnosis.innerHTML = data.applies[i].diagnosis;
+        text += "<li>" + data.applies[i].date + " " + data.applies[i].time + "<br>";
+        text += "<strong> Diagnosis</strong>: " + data.applies[i].diagnosis + "<br>";
+
+        text += "<strong> Symptoms: </strong><br> ";
+        text += "<ul>";
+        var sympts = data.applies[i].symptoms.split(" ");
+        var symptst = data.applies[i].symptoms_text.split(".");
+        for (let j = 0; j < sympts.length; ++j) {
+            var sid = sympts[j];
+            text += "<li>" + symptst[j] + "</li>";
+        }
+
+        text += "</ul></li>";
     }
+    applies.innerHTML = text;
+
+    list.appendChild(applies);
 }
 
 function get_patient_info(div_id) {
     var patient_id = document.getElementById('patient_id').value;
-    /*axios.get("https://5d11ebed.ngrok.io/api/patients_history?id=" + patient_id).then(
+    axios.get("https://5d11ebed.ngrok.io/api/patients_history?id=" + patient_id).then(
         response => {
             show_history(response.data, div_id);
-        });*/
-    show_history({"id":1,"name":"Alexander Smirnov","gender":"female","year_of_birth":2000,"applies":[{"date":"2019-03-30","time":"23:59:47.265702","symptoms":"13 15 95","diagnosis":"<strong>Cold</strong> <i>(Acute nasopharyngitis [common cold])</i>"}]}, "patient-info");
+        });
+    //show_history({"id":1,"name":"Alexander Smirnov","gender":"female","year_of_birth":2000,"applies":[{"date":"2019-03-30","time":"23:59:47.265702","symptoms":"13 15 95","diagnosis":"<strong>Cold</strong> <i>(Acute nasopharyngitis [common cold])</i>"}]}, div_id);
 }
