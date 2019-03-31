9eee9807214f7b9856a80a3e0b048a2414632afc
AlexFSmirnov <alex.f.smirnov@gmail.com>
2019-03-31 09:00:31 +0200

    NO MORE GENDERS

--- doctors_front/js/script.js
+++ doctors_front/js/script.js
@@ -8,7 +8,7 @@ function show_history(data, div_id) {
         params={"patient_id": data.id, "message": msg}).then(r => {console.log(r); console.log(msg + " SENT!");});
 
     var general = document.createElement('div');
-    general.innerHTML = "<strong>" + data.name + "</strong>, " + data.gender + ", " + data.year_of_birth + " year of birth.<br>Phone number: " + data.phone + "<br><br>";
+    general.innerHTML = "<strong>" + data.name + "</strong>, " + data.year_of_birth + " year of birth.<br>Phone number: " + data.phone + "<br><br>";
 
     var applied_on = document.createElement('div');
     applied_on.innerHTML = "Patient applied " + data.applies.length + " time" + (data.applies.length == 1 ? "" : "s") + ": ";
